12a71a2e021ecd698ecf5e9b16dfe701ef8d0d75
anioutkazharkova <anioutka-jarkova@yandex.ru>
2014-12-07 19:40:02 +0600

    custom search view

---
+++ CustomizedSearchView/src/com/anioutkazharkova/customizedsearchview/views/MySearchView.java
@@ -0,0 +1,74 @@
+package com.anioutkazharkova.customizedsearchview.views;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SearchView;
+
+public class MySearchView extends SearchView {
+
+	 private Typeface mFont;
+	 private Context mContext;
+	 
+	 private View mSearchEditFrame;
+	 private View mSearchPlate;
+	 private View mSubmitArea;
+	 private ImageView mSearchButton;
+	 private ImageView mSubmitButton;
+	 private ImageView mCloseButton;
+	 private ImageView mVoiceButton;
+	 private ImageView mSearchHintIcon;
+	 
+	 
+	public MySearchView(Context context) {
+		super(context);
+		mContext=context;
+		
+		initFields();
+		
+		
+	}
+	public MySearchView(Context context,AttributeSet attrs)
+	{
+		super(context,attrs);
+		mContext=context;
+		
+		initFields();
+	}
+	
+	public void setTypeFace(Typeface font)
+	{
+		this.mFont=font;
+	}
+
+	private void initFields()
+	{
+		int editFrameId=mContext.getResources().getIdentifier("android:id/search_src_text", null, null);
+		mSearchEditFrame=findViewById(editFrameId);
+		
+		int plateId=mContext.getResources().getIdentifier("android:id/search_plate", null, null);
+		mSearchPlate=findViewById(plateId);
+		
+		int areaId=mContext.getResources().getIdentifier("android:id/submit_area", null, null);
+		mSubmitArea=findViewById(areaId);
+		
+		int subBtnId=mContext.getResources().getIdentifier("android:id/search_go_btn", null, null);
+		mSubmitButton=(ImageView) findViewById(subBtnId);
+		
+		int closeBtnId=mContext.getResources().getIdentifier("android:id/search_close_btn", null, null);
+		mCloseButton=(ImageView) findViewById(closeBtnId);
+		
+		
+		/*mQueryTextView = (SearchAutoComplete) findViewById(R.id.search_src_text);		
+		mSearchPlate = findViewById(R.id.search_plate);
+		mSubmitArea = findViewById(R.id.submit_area);
+		mSubmitButton = findViewById(R.id.search_go_btn);
+		mCloseButton = (ImageView) findViewById(R.id.search_close_btn);
+		mVoiceButton = findViewById(R.id.search_voice_btn);
+		mSearchHintIcon = (ImageView) findViewById(R.id.search_mag_icon); */
+	}
+	
+	
+}
