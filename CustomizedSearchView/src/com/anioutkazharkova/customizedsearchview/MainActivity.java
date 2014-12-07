4f61a1928f1da9dbb6fb00db107658d11094d4d2
anioutkazharkova <anioutka-jarkova@yandex.ru>
2014-12-07 21:23:43 +0600

    searchview color customization

--- CustomizedSearchView/src/com/anioutkazharkova/customizedsearchview/MainActivity.java
+++ CustomizedSearchView/src/com/anioutkazharkova/customizedsearchview/MainActivity.java
@@ -1,36 +1,31 @@
 package com.anioutkazharkova.customizedsearchview;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+
+import com.anioutkazharkova.customizedsearchview.views.MySearchView;
 
 
 public class MainActivity extends Activity {
 
+	MySearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        searchView=(MySearchView)findViewById(R.id.searchView);
+       
+       searchView.setTextSize(20);
+       searchView.setFont(Typeface.createFromAsset(getAssets(), "font.ttf"),Typeface.ITALIC);
+       searchView.setTextStyle(getResources().getColor(R.color.text_white), getResources().getColor(R.color.hint_gray));
+       searchView.setHintStyle("Enter text", R.drawable.search_light_icon);
+       searchView.setBackgroundFrame(R.drawable.editframe);
+       
+       searchView.getSearchIcon().setVisibility(View.GONE);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+  
 }
--- CustomizedSearchView/src/com/anioutkazharkova/customizedsearchview/views/MySearchView.java
+++ CustomizedSearchView/src/com/anioutkazharkova/customizedsearchview/views/MySearchView.java
@@ -1,11 +1,18 @@
 package com.anioutkazharkova.customizedsearchview.views;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ImageSpan;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 public class MySearchView extends SearchView {
 
@@ -13,6 +20,7 @@ public class MySearchView extends SearchView {
 	 private Context mContext;
 	 
 	 private View mSearchEditFrame;
+	 private EditText mSearchEditText;
 	 private View mSearchPlate;
 	 private View mSubmitArea;
 	 private ImageView mSearchButton;
@@ -38,15 +46,69 @@ public class MySearchView extends SearchView {
 		initFields();
 	}
 	
-	public void setTypeFace(Typeface font)
+	public void setFont(Typeface font,int style)
 	{
 		this.mFont=font;
+		mSearchEditText.setTypeface(font,style);
+	}
+	public void setTextSize(float textSize)
+	{
+		mSearchEditText.setTextSize(textSize);
+	}
+	public void setTextStyle(int color,int hintColor)
+	{
+		mSearchEditText.setTextColor(color);
+		mSearchEditText.setHintTextColor(hintColor);
+		
+	}
+	
+	public void setHintStyle(String hintText,int icon)
+	{
+		mSearchEditText.setHint(changeSpannableIcon(hintText, icon));
+	}
+	public void setBackgroundFrame(int backgroundFrame)
+	{
+		
+		mSearchPlate.setBackgroundResource(backgroundFrame);
+	}
+	private SpannableStringBuilder changeSpannableIcon(String hintText, int icon)
+    {
+        
+        SpannableStringBuilder ssb = new SpannableStringBuilder("  ");
+        ssb.append(hintText);
+        
+        Drawable searchIcon = mContext.getResources().getDrawable(icon);
+        int textSize = (int)(mSearchEditText.getTextSize());
+        searchIcon.setBounds(0, 0, textSize, textSize);
+        ssb.setSpan(new ImageSpan(searchIcon),1, 2,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        
+        return ssb;
+    }
+	
+	public ImageView getSearchIcon()
+	{
+		return mSearchHintIcon;
+	}
+	public ImageView getCloseButton()
+	{
+		return mCloseButton;
+	}
+	public ImageView getSubmitButton()
+	{
+		return mSearchButton;
+	}
+	public ImageView getVoiceButton()
+	{
+		return mVoiceButton;
 	}
 
 	private void initFields()
 	{
-		int editFrameId=mContext.getResources().getIdentifier("android:id/search_src_text", null, null);
-		mSearchEditFrame=findViewById(editFrameId);
+		int editframeId=mContext.getResources().getIdentifier("android:id/search_edit_frame", null, null);
+		mSearchEditFrame=findViewById(editframeId);
+		
+		int textId=mContext.getResources().getIdentifier("android:id/search_src_text", null, null);
+		mSearchEditText=(EditText) findViewById(textId);
 		
 		int plateId=mContext.getResources().getIdentifier("android:id/search_plate", null, null);
 		mSearchPlate=findViewById(plateId);
